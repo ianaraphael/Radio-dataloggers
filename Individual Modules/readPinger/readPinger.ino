99cb7bc28041ed07c382eb102e1185b33d7ef139
ianaraphael <ian.a.raphael.th@dartmouth.edu>
2021-07-19 13:59:00 -0400

    Reading snow pinger digital output and error checking the string.

---
+++ Individual Modules/readPinger/readPinger.ino
@@ -0,0 +1,81 @@
+/*
+
+readPinger.ino
+
+Read and print snow pinger digital output with output error checking and non-return
+protection
+
+Ian Raphael
+ian.th@dartmouth.edu
+2021.07.19
+
+*/
+
+#define Serial SerialUSB
+#define PINGER_BUS Serial1
+#define PINGER_TIMEOUT 100
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("USB Comms online");
+
+  PINGER_BUS.begin(9600);
+}
+
+void loop() {
+  // If we're connected to the PINGER_BUS get a reading. Since the pinger is constantly
+  // spewing data when powered we only need the RX pin (we never TX to pinger)
+  if (PINGER_BUS.available()) {
+
+    String pingerReadout; // a string to hold what's read off the pinger
+    String pingerReturn; // a string to return the cleaned pinger info
+
+    // TODO: Include a timer here to protect against errors. We don't want to get into
+    // a situation where the pinger is just spitting back garbage (so Serial1.available() still
+    // returns true) but this just hangs the board since it never spits out '\r'
+    // and possibly runs us out of memory.
+    // set a timeout for reading the serial line
+    Serial.setTimeout(PINGER_TIMEOUT);
+
+    // The format of the pingers output is ascii: Rxxxx\r where x is a digit
+    // Thus we can read the streaming input until we catch '\r'
+    // get the pinger readout
+    pingerReadout = PINGER_BUS.readStringUntil('\r');
+
+    // copy the pinger readout into the return string starting after the first char
+    pingerReturn = pingerReadout.substring(1);
+
+    // if the readout doesn't conform to the R + 4 digits format
+    if ((pingerReadout[0] != 'R') || (pingerReadout.length()!= 5)) { // first check if the first char is caps R
+
+      // set the output to string "NaN"
+      pingerReturn = "NaN";
+
+    } else {
+      // then for the following four chars
+      for (int i=1; i<5; i++) {
+
+        // if any of them aren't digits
+        if (!isdigit(pingerReadout[i])) {
+
+          // set the output to string "NaN"
+          pingerReturn = "NaN";
+
+          // and escape
+          break;
+        }
+      }
+    }
+
+    Serial.print("Digital read: ");
+    Serial.println(pingerReturn);
+  }
+
+  // Serial.print(" ");
+  // The brown wire outputs the distance in analog. Using the arduino's native
+  // 10 bit adc resolution the distance is approximately the analog value * 5
+  // Serial.println(analogRead(A1)*5);
+  delay(500);
+
+}
--- Legacy code (MOSAiC)/Pinger/Pinger.ino
+++ Legacy code (MOSAiC)/Pinger/Pinger.ino
@@ -1,18 +1,20 @@
 /*
  * provides a brief example of reading the serial output and analog output of a maxbotix pinger
  * !!!!!!may only work with mini ultra pro board
- * 
+ *
  * Wires coming out of pinger:
  * Red: 3V3 (Vin)
  * Black: GND
  * Brown: A1 outputs distance as scaled Analog voltage between GND and Vin (Vin corresponds to 4999 mm)
- * Blue: 0 (Serial1's RX)
+ * Blue: 0 (PINGER_BUS's RX)
  * White: Not used, probably pin 1 temperature sensor input
  * Orange: not used, PWM output
  * Green: not used, pull low to turn off, pull high to turn on. Internally pulled high.
  */
 
 #define Serial SerialUSB
+#define PINGER_BUS Serial
+
 
 String pingerOutput;
 
@@ -21,13 +23,13 @@ void setup() {
   while(!Serial);
   Serial.println("USB Comms onine");
 
-  Serial1.begin(9600);
+  PINGER_BUS.begin(9600);
 }
 
 void loop() {
-  // If we're connected to the Serial1 get a reading. Since the pinger is constantly 
+  // If we're connected to the PINGER_BUS get a reading. Since the pinger is constantly
   // spewing data when powered we only need the RX pin (we never TX to pinger)
-  if (Serial1.available()) {
+  if (PINGER_BUS.available()) {
     // The format of the pingers output is ascii: Rxxxx\r where x is a digit
     // Thus we can read the streaming input until we catch '\r'
     // TODO: check that string matches 'R'[0-9][0-9][0-9][0-9]'\r' and if not discard it
@@ -35,10 +37,10 @@ void loop() {
     // a situation where the pinger is just spitting back garbage (so Seral1.available() still
     // returns true) but this just hangs the microcontroller since it never spits out '\r'
     // and possibly runs us out of memory.
-    pingerOutput = Serial1.readStringUntil('\r');
+    pingerOutput = PINGER_BUS.readStringUntil('\r');
     Serial.print(pingerOutput);
   }
-  
+
   Serial.print(" ");
   // The brown wire outputs the distance in analog. Using the arduino's native
   // 10 bit adc resolution the distance is approximately the analog value * 5
