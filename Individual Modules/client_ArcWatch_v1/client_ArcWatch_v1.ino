d8706381f40d9bcd1070eb4c2f8535d8d97b46bb
ianaraphael <ian.a.raphael.th@dartmouth.edu>
2023-12-18 11:21:13 -0500

    fully updated but untested server/node code for new mini ultra boards

--- Individual Modules/client_ArcWatch_v1/client_ArcWatch_v1.ino
+++ Individual Modules/client_ArcWatch_v1/client_ArcWatch_v1.ino
@@ -33,7 +33,7 @@ void setup() {
   // init the realtime clock
   init_RTC();
 
-  // set the radio pin
+  // TODO: set the radio pin
   pinMode(4, OUTPUT);
   digitalWrite(4, HIGH);
   // init the radio
@@ -42,67 +42,82 @@ void setup() {
   Serial.print("Station #");
   Serial.print(STATION_ID,DEC);
   Serial.println(" init'd");
+
+  // set this false on startup so that any time we black out we reattempt sync
+  syncedWithServer = false;
 }
 
 
 
 void loop() {
 
-  // set the radio pin
+  // TODO: set the radio pin
   pinMode(4, OUTPUT);
   digitalWrite(4, HIGH);
 
   // init the radio
   init_Radio();
 
-  // attempt to sync up with the server
-  syncedWithServer = attemptSyncWithServer();
+  // while we're not synced up with the server
+  while (!syncedWithServer) {
 
-  // if we're synced up with the server
-  if (syncedWithServer) {
+    // attempt to sync up with the server
+    syncedWithServer = attemptSyncWithServer();
 
-    // set the next alarm
-    setSleepAlarm();
-
-    // read the data
-    uint8_t pingerData = readPinger();
-    static float tempData[NUM_TEMP_SENSORS];
-    readTemps(tempData);
-
-    // print out the temp data
-    Serial.println("temp data: ");
-    for (int i=0;i<NUM_TEMP_SENSORS;i++) {
-      Serial.println(tempData[i],3);
-    }
-    Serial.println("pinger data: ");
-    Serial.println(pingerData);
-
-    // allocate a buffer
-    uint8_t dataBuffer[CLIENT_DATA_SIZE];
-
-    // and pack it
-    packClientData(tempData, pingerData, dataBuffer);
-
-    Serial.println("Here's the data we're sending: ");
-    for (int i=0;i<CLIENT_DATA_SIZE;i++){
-      Serial.print(" 0x");
-      Serial.print(dataBuffer[i],HEX);
-    }
-    Serial.println("");
-
-    // delay for a couple of second
-    delay(CLIENT_TRANSMIT_DELAY_SECS*1000);
-
-    // send the data
-    Serial.println("Attempting radio transmit");
-
-    if(manager.sendtoWait(dataBuffer,CLIENT_DATA_SIZE, SERVER_ADDRESS)){
-      Serial.println("Successful transmit");
-    } else {
-      Serial.println("Failed transmit");
+    // if successful
+    if (syncedWithServer){
+      // break out
+      break;
     }
 
-    // go to sleep
-    LowPower.standby();
+    // otherwise go to sleep for an interval half as long as the server wake duration
+    if (SERVER_WAKE_DURATION > 1) {
+      setSleepAlarm(SERVER_WAKE_DURATION/2);
+      LowPower.standby();
+    }
   }
+
+  // set the next sampling alarm
+  setSleepAlarm(SAMPLING_INTERVAL_MIN);
+
+  // read the data
+  uint8_t pingerData = readPinger();
+  static float tempData[NUM_TEMP_SENSORS];
+  readTemps(tempData);
+
+  // print out the temp data
+  Serial.println("temp data: ");
+  for (int i=0;i<NUM_TEMP_SENSORS;i++) {
+    Serial.println(tempData[i],3);
+  }
+  Serial.println("pinger data: ");
+  Serial.println(pingerData);
+
+  // allocate a buffer
+  uint8_t dataBuffer[CLIENT_DATA_SIZE];
+
+  // and pack it
+  packClientData(tempData, pingerData, dataBuffer);
+
+  Serial.println("Here's the data we're sending: ");
+  for (int i=0;i<CLIENT_DATA_SIZE;i++){
+    Serial.print(" 0x");
+    Serial.print(dataBuffer[i],HEX);
+  }
+  Serial.println("");
+
+  // delay for a couple of second
+  delay(CLIENT_TRANSMIT_DELAY_SECS*1000);
+
+  // send the data
+  Serial.println("Attempting radio transmit");
+
+  if(manager.sendtoWait(dataBuffer,CLIENT_DATA_SIZE, SERVER_ADDRESS)){
+    Serial.println("Successful transmit");
+  } else {
+    Serial.println("Failed transmit");
+  }
+
+  // go to sleep
+  LowPower.standby();
 }
--- Individual Modules/serverArcWatch_v2/serverArcWatch_v2.ino
+++ Individual Modules/serverArcWatch_v2/serverArcWatch_v2.ino
@@ -1,5 +1,3 @@
-// server
-
 /*
 
 server_ArcWatch_v2
@@ -7,22 +5,26 @@ server_ArcWatch_v2
 bytestream based server for ArcWatch deployment. Eliminates local files, human
 readable text.
 
-standalone iridium service
+updated for new ultrascream board
 
 Ian Raphael
 ian.a.raphael.th@dartmouth.edu
-2023.07.27
+2023.12.01
 
 */
 
+
+// **************************** defines, macros **************************** //
 #define Serial SerialUSB // comment if not using rocketscream boards
 
 #include "SnowTATOS.h"
 #include "SnowTATOS_i2c.h"
 
-// declare a buffer to hold simb data (we're actually just going to use this to transfer via iridium)
+// declare a buffer to hold simb data
 uint8_t simbData[SIMB_DATASIZE];
 
+
+// ********************************* setup ********************************* //
 void setup() {
 
   // Begin serial comms
@@ -34,16 +36,8 @@ void setup() {
   // init the radio
   init_Radio();
 
-  // init the realtime clock
+  // init the realtime counter
   init_RTC();
-
-  Serial.print("init time (hour,min,sec): ");
-  Serial.print(sc_RTC.getHours(),DEC);
-  Serial.print(", ");
-  Serial.print(sc_RTC.getMinutes(),DEC);
-  Serial.print(", ");
-  Serial.println(sc_RTC.getSeconds(),DEC);
-
 
   // wipe the simb buffer
   memset(simbData,0, sizeof(simbData));
@@ -53,167 +47,94 @@ void setup() {
 
   // print success
   Serial.println("Server init success");
-
-  // if it's not a test
-  if (!TEST) {
-
-    // we're going to sleep
-    Serial.println("Going to sleep");
-
-    bool firstAlarm=true;
-    // set the first alarm for midnight
-    setAlarm(firstAlarm);
-
-    Serial.print("First alarm set for (hour, min): ");
-    Serial.print(ALARM_HOURS,DEC);
-    Serial.print(", ");
-    Serial.println(ALARM_MINUTES,DEC);
-
-    delay(1000);
-
-    // and go to sleep
-    sc_RTC.standbyMode();
-  }
 }
 
 
-
+// ******************************* main loop ******************************* //
 void loop() {
 
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH);
+  if (justWokeUp) {
 
-  // Serial.print("current time: ");
-  // Serial.print(sc_RTC.getHours(),DEC);
-  // Serial.print(", ");
-  // Serial.print(sc_RTC.getMinutes(),DEC);
-  // Serial.print(", ");
-  // Serial.println(sc_RTC.getSeconds(),DEC);
+    setWakeAlarm(SERVER_WAKE_DURATION);
 
-  // ********** radio comms with clients ********** //
+    // TODO: radio?
+    pinMode(4, OUTPUT);
+    digitalWrite(4, HIGH);
+  }
 
-  // allocate a buffer to hold data from a client (with some padding)
-  uint8_t currData[CLIENT_DATA_SIZE];
+  if (!timeToSleep) {
 
-  // check for a radio transmission from a client
-  int stnID = receiveData_fromClient(currData);
+    // allocate a broadcast buffer
+    broadcastBuffer[2];
 
-  // if we got data
-  if (stnID != -1) {
+    // put our address in it
+    broadcastBuffer[0] = SERVER_ADDRESS;
 
-    Serial.print("Received data from stn ");
-    Serial.println(stnID);
-    Serial.println("");
+    // broadcast that we're awake
+    sendtoWait(broadcastBuffer,sizeof(broadcastBuffer),RH_BROADCAST_ADDRESS);
 
-    Serial.println("Raw data: ");
-    for (int i=0; i<CLIENT_DATA_SIZE;i++){
-      Serial.print(" 0x");
-      Serial.print(currData[i],HEX);
+    // ************************ client radio comms ************************ //
+
+    // allocate a buffer to hold data from a client
+    uint8_t currData[CLIENT_DATA_SIZE];
+
+    // check for a radio transmission from a client
+    int stnID = receiveData_fromClient(currData);
+
+    // if we got data
+    if (stnID != -1) {
+
+      Serial.print("Received data from stn ");
+      Serial.println(stnID);
+      Serial.println("");
+
+      Serial.println("Raw data: ");
+      for (int i=0; i<CLIENT_DATA_SIZE;i++){
+        Serial.print(" 0x");
+        Serial.print(currData[i],HEX);
+      }
+      Serial.println("");
+
+      // find the start byte in the simb buffer for this station
+      int startByte = (stnID-1)*CLIENT_DATA_SIZE;
+
+      // write the data to the buffer
+      for (int i = startByte;i<startByte+CLIENT_DATA_SIZE;i++) {
+        simbData[i] = currData[i-startByte];
+      }
+
+      float temps[3];
+      unpackTempData(simbData,temps,stnID);
+      uint8_t pingerValue = unpackPingerData(simbData,stnID);
+
+      Serial.println("  Temps:");
+      for (int i=0; i<3;i++){
+        Serial.println(temps[i],3);
+      }
+      Serial.println("  Pinger:");
+      Serial.println(pingerValue);
     }
-    Serial.println("");
-
-    // find the start byte in the simb buffer for this station
-    int startByte = (stnID-1)*CLIENT_DATA_SIZE;
-
-    // write the data to the buffer
-    for (int i = startByte;i<startByte+CLIENT_DATA_SIZE;i++) {
-      simbData[i] = currData[i-startByte];
-    }
-
-    float temps[3];
-    unpackTempData(simbData,temps,stnID);
-    uint8_t pingerValue = unpackPingerData(simbData,stnID);
-
-    Serial.println("  Temps:");
-    for (int i=0; i<3;i++){
-      Serial.println(temps[i],3);
-    }
-    Serial.println("  Pinger:");
-    Serial.println(pingerValue);
   }
 
 
-  // ********** iridium transmit and sleep ********** //
+  // ***************************** SIMB comms ***************************** //
 
-  // if we've been awake for long enough (or we're testing)
-  int elapsedTime_mins = (millis() - wokeUpAtMillis)/60000;
-  if ((elapsedTime_mins >= SERVER_WAKE_DURATION) or TEST) {
+  // if the simb has requested data
+  if (simbRequestedData()) {
+    // send the data
+    sendDataToSimb(simbData);
+  }
 
-    // write the data to the message
-    for (int i=0;i<SIMB_DATASIZE;i++){
-      message.data[i] = simbData[i];
-    }
 
-    // write the timestamp to the message
-    message.timestamp = sc_RTC.getEpoch();
+  // ******************************* sleep ******************************* //
 
-    // if iridium is enabled
-    if (IRIDIUM_ENABLE) {
+  // if it's time to go to sleep
+  if (timeToSleep) {
 
-      // Transmit over Iridium
-      Serial.println("\nTransmitting on Iridium...");
+    // set the alarm
+    setSleepAlarm(SAMPLING_INTERVAL_MIN);
 
-      // turn the iridium unit on
-      iridiumOn();
-      Serial.println("Iridium is on.");
-
-      // send the data
-      sendIridium();
-
-      // turn the iridium unit off
-      iridiumOff();
-      Serial.println("Done transmitting...");
-
-      // print error status
-      Serial.print("Iridium status: ");
-      Serial.println(iridiumError);
-
-      // now reset the buffer to error vals
-      maskSimbData(simbData);
-
-      // clear the sbd message
-      clearMessage();
-
-      // we're going to sleep
-      Serial.println("Going to sleep");
-
-      bool firstAlarm = false;
-      // schedule the next sleep alarm normally
-      setAlarm(firstAlarm);
-
-      Serial.print("Next alarm set for (hour, min): ");
-      Serial.print(ALARM_HOURS,DEC);
-      Serial.print(", ");
-      Serial.println(ALARM_MINUTES,DEC);
-
-      // and go to sleep
-      sc_RTC.standbyMode();
-
-      // otherwise
-    } else {
-
-      // now reset the buffer to error vals
-      maskSimbData(simbData);
-
-      // clear the sbd message
-      clearMessage();
-
-      // we're going to sleep
-      Serial.println("Going to sleep");
-
-      bool firstAlarm = false;
-      // schedule the next sleep alarm normally
-      setAlarm(firstAlarm);
-
-      Serial.print("Next alarm set for (hour, min): ");
-      Serial.print(ALARM_HOURS,DEC);
-      Serial.print(", ");
-      Serial.println(ALARM_MINUTES,DEC);
-
-      delay(1000);
-
-      // and go to sleep
-      sc_RTC.standbyMode();
-    }
+    // and go to sleep
+    LowPower.standby();
   }
 }
