465d2ee15c990de8a69f5b8bdbcb1e2f9770b8f2
ianaraphael <ian.a.raphael.th@dartmouth.edu>
2023-12-20 10:13:02 -0500

    successful server integration into barebones simb program and full simb suite with dummy buffer

--- Individual Modules/serverArcWatch_v2/serverArcWatch_v2.ino
+++ Individual Modules/serverArcWatch_v2/serverArcWatch_v2.ino
@@ -15,7 +15,7 @@ ian.a.raphael.th@dartmouth.edu
 
 
 // **************************** defines, macros **************************** //
-#define Serial SerialUSB // comment if not using rocketscream boards
+#define Serial Serial2 // comment if not using rocketscream boards
 
 #include "SnowTATOS.h"
 #include "SnowTATOS_i2c.h"
@@ -28,13 +28,14 @@ uint8_t simbData[SIMB_DATASIZE];
 void setup() {
 
   // Begin serial comms
+  Serial.swap(1);
   Serial.begin(9600);
   delay(10000);
 
   boardSetup();
 
   // init the radio
-  init_Radio();
+  // init_Radio();
 
   // init the realtime counter
   init_RTC();
@@ -44,6 +45,8 @@ void setup() {
 
   // and paint error vals in
   maskSimbData(simbData);
+
+  init_I2C_scSide();
 
   // print success
   Serial.println("Server init success");
@@ -58,62 +61,62 @@ void loop() {
     setWakeAlarm(SERVER_WAKE_DURATION);
 
     // TODO: radio?
-    pinMode(4, OUTPUT);
-    digitalWrite(4, HIGH);
+    // pinMode(4, OUTPUT);
+    // digitalWrite(4, HIGH);
   }
 
   if (!timeToSleep) {
 
     // allocate a broadcast buffer
-    broadcastBuffer[2];
+    int broadcastBuffer[2];
 
     // put our address in it
     broadcastBuffer[0] = SERVER_ADDRESS;
 
-    // broadcast that we're awake
-    sendtoWait(broadcastBuffer,sizeof(broadcastBuffer),RH_BROADCAST_ADDRESS);
-
-    // ************************ client radio comms ************************ //
-
-    // allocate a buffer to hold data from a client
-    uint8_t currData[CLIENT_DATA_SIZE];
-
-    // check for a radio transmission from a client
-    int stnID = receiveData_fromClient(currData);
-
-    // if we got data
-    if (stnID != -1) {
-
-      Serial.print("Received data from stn ");
-      Serial.println(stnID);
-      Serial.println("");
-
-      Serial.println("Raw data: ");
-      for (int i=0; i<CLIENT_DATA_SIZE;i++){
-        Serial.print(" 0x");
-        Serial.print(currData[i],HEX);
-      }
-      Serial.println("");
-
-      // find the start byte in the simb buffer for this station
-      int startByte = (stnID-1)*CLIENT_DATA_SIZE;
-
-      // write the data to the buffer
-      for (int i = startByte;i<startByte+CLIENT_DATA_SIZE;i++) {
-        simbData[i] = currData[i-startByte];
-      }
-
-      float temps[3];
-      unpackTempData(simbData,temps,stnID);
-      uint8_t pingerValue = unpackPingerData(simbData,stnID);
-
-      Serial.println("  Temps:");
-      for (int i=0; i<3;i++){
-        Serial.println(temps[i],3);
-      }
-      Serial.println("  Pinger:");
-      Serial.println(pingerValue);
-    }
+    // // broadcast that we're awake
+    // sendtoWait(broadcastBuffer,sizeof(broadcastBuffer),RH_BROADCAST_ADDRESS);
+    //
+    // // ************************ client radio comms ************************ //
+    //
+    // // allocate a buffer to hold data from a client
+    // uint8_t currData[CLIENT_DATA_SIZE];
+    //
+    // // check for a radio transmission from a client
+    // int stnID = receiveData_fromClient(currData);
+    //
+    // // if we got data
+    // if (stnID != -1) {
+    //
+    //   Serial.print("Received data from stn ");
+    //   Serial.println(stnID);
+    //   Serial.println("");
+    //
+    //   Serial.println("Raw data: ");
+    //   for (int i=0; i<CLIENT_DATA_SIZE;i++){
+    //     Serial.print(" 0x");
+    //     Serial.print(currData[i],HEX);
+    //   }
+    //   Serial.println("");
+    //
+    //   // find the start byte in the simb buffer for this station
+    //   int startByte = (stnID-1)*CLIENT_DATA_SIZE;
+    //
+    //   // write the data to the buffer
+    //   for (int i = startByte;i<startByte+CLIENT_DATA_SIZE;i++) {
+    //     simbData[i] = currData[i-startByte];
+    //   }
+    //
+    //   float temps[3];
+    //   unpackTempData(simbData,temps,stnID);
+    //   uint8_t pingerValue = unpackPingerData(simbData,stnID);
+    //
+    //   Serial.println("  Temps:");
+    //   for (int i=0; i<3;i++){
+    //     Serial.println(temps[i],3);
+    //   }
+    //   Serial.println("  Pinger:");
+    //   Serial.println(pingerValue);
+    // }
   }
 
 
